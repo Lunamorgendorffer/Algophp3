5093e9f5913498f5a29d75fb506c6e287e4de526
Lunamorgendorffer <nabdou68@gmail.com>
2023-02-15 11:46:30 +0100

    ajout de la fonction pr afficher chambres

--- Hotel/index.php
+++ Hotel/index.php
@@ -21,13 +21,20 @@ $hotel2= new Hotel("Regent","****","Paris","61 Rue Dauphine 75006 Paris");
 $client1= new Client ("Virgile","GIBELLO");
 $client2= new Client ("Micka","MURMANN");
 
-$chambreA= new Chambre ("17","1",TRUE,$hotel1,300);
-$chambreB= new Chambre ("3","2",false,$hotel1,120);
-$chambreC= new Chambre ("4","2",false,$hotel1,120);
+$chambre1= new Chambre ("1","2",false,$hotel1,120,false);
+$chambre2= new Chambre ("2","2",false,$hotel1,120,false);
+$chambreA= new Chambre ("17","1",TRUE,$hotel1,300,true);
+$chambreB= new Chambre ("3","2",false,$hotel1,120,true);
+$chambreC= new Chambre ("4","2",false,$hotel1,120,true);
+$chambre16= new Chambre ("16","2",true,$hotel1,300,false);
+$chambre18= new Chambre ("18","2",true,$hotel1,300,false);
+$chambre19= new Chambre ("19","2",true,$hotel1,300,false);
 
 $resa1= new Reservation("01-01-2021","01-01-2021",$client1,$hotel1,$chambreA);
 $resa2= new Reservation("11-03-2021","11-03-2021",$client2,$hotel1,$chambreB);
 $resa3= new Reservation("01-04-2021","01-04-2021",$client2,$hotel1,$chambreC);
+
+$chambres = array($chambre1,$chambre2,$chambreA, $chambreB,$chambreC, $chambre16,$chambre18,$chambre19);
 
 
 
@@ -43,7 +50,7 @@ $resa3= new Reservation("01-04-2021","01-04-2021",$client2,$hotel1,$chambreC);
 <div>
     <h3>Reservation de l'hotel <?php echo $hotel1->getNomHotel().$hotel1->get_nbEtoile().$hotel1->get_ville() ?></h3>
     <p uk-margin>
-    <button class="uk-button uk-button-primary uk-button-small"><?php echo count($hotel1->get_reservations())?> Réservations</button>
+    <button class="uk-button uk-button-primary uk-button-small"><?php echo $hotel1->get_reservations()?> Réservations</button>
     </p>
     <div>
     <?php 
@@ -56,7 +63,7 @@ $resa3= new Reservation("01-04-2021","01-04-2021",$client2,$hotel1,$chambreC);
 <div>
     <h3>Reservation de l'hotel <?php echo $hotel2->getNomHotel().$hotel2->get_nbEtoile().$hotel2->get_ville() ?></h3>
     <div>
-    <?php echo $hotel2->nbReservations()?>
+    <?php echo $hotel2->get_reservations()?>
     </div>
 </div>
 
@@ -71,9 +78,36 @@ $resa3= new Reservation("01-04-2021","01-04-2021",$client2,$hotel1,$chambreC);
         <?php echo $chambreB->afficherChambre();?><br>
         <strong>Hotel <?php echo $hotel1->getNomHotel().$hotel1->get_nbEtoile().$hotel1->get_ville() ?></strong> /
         <?php echo $chambreC->afficherChambre();?><br>
-        Total:
-        </p>
+        Total: <?= $client2->totalReservations();?> €
+        </p><br>
     </div>
+</div>
+<div>
+    <p>Statut des chambres de <strong><?php echo $hotel1->getNomHotel().$hotel1->get_nbEtoile().$hotel1->get_ville() ?></strong> </p>
+    <table class="uk-table uk-table-striped">
+        <thead>
+            <tr>
+                <th>CHAMBRE</th>
+                <th>PRIX</th>
+                <th>WIFI</th>
+                <th>ETAT</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+
+            foreach ($chambres as $chambre){
+                echo "<tr>",
+                        "<td> Chambre " . $chambre->get_numChambre() . "</td>",
+                        "<td>" . $chambre->get_prix() . "</td>",
+                        "<td>" . $chambre->get_wifi() . "</td>",
+                        "<td>" . $chambre->get_statut() . "</td>",
+                    "</tr>";
+            } 
+            ?>
+        </tbody>
+    </table>
+
 </div>
 
 </body>
